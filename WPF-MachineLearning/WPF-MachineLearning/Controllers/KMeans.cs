10e0ae780dc09dba9a12b1b0609b16fd4b378f1d
StephenGoedhart <sdg25@hotmail.com>
2019-03-19 15:22:20 +0100

    initalcommit

---
+++ WPF-MachineLearning/WPF-MachineLearning/Controllers/KMeans.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MachineLearning.Controllers
+{
+    class WhineOrder{
+        // Offer, Campaign, Varietal, Minimum, Origin, PastPeak
+        public int[] values { get; private set; }
+
+        public WhineOrder(string[] values)
+        {
+                
+        }
+        
+        static CampaignDistance()
+        {
+
+        }
+
+
+
+
+    }
+
+    class KMeans
+    {
+        public KMeans()
+        {
+        
+        }
+
+    }
+}
